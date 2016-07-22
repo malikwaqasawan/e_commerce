a12455b5affcde8f24b60ec252c1a21f698d43fe
Waqas Awan <waqas487@outlook.com>
2016-07-22 08:15:37 -0400

    Products: action controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,54 @@
 class ProductsController < ApplicationController
 
+	before_action :set_product, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@products = Product.all
 	end
 
 	def show
+	end
+
+	def edit
+	end
+
+	def update
+
+		if @product.update_attributes(product_params)
+			redirect_to products_path, notice: "Product Updated Successfully"
+		else
+			render "edit"
+		end
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+
+		if @product.save
+			redirect_to products_path, notice: "Product Created Successfully"
+		else
+			render "new"
+		end
+	end
+
+	def destroy
+		@product.destroy
+
+		redirect_to products_path, notice: "Product Deleted Successfully"
+	end
+
+
+	private
+
+	def set_product
 		@product = Product.find(params[:id])
 	end
 
+	def product_params
+		params.require(:product).permit(:name, :quantity, :price)
+	end
 end
