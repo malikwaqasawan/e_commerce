9dc3ca99385e3aff4d8590825441dabdf9b88e7a
Waqas Awan <waqas487@outlook.com>
2016-07-27 06:01:01 -0400

    Style: Added styling to products and devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 
+
   resources :products do
-  resources :variants, shallow: true
-end
+    resources :variants, shallow: true
+  end
+
+  root to: 'products#index'
 
 end
